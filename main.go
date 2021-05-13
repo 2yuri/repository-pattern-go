bb2110373bdea4952fb743a8f004ee4326d23542
Yuri Alves Miguel <yuri.snp@hotmail.com>
2021-05-12 21:12:36 -0300

    fix main.go

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/hyperyuri/repository-pattern-go/database"
 	"github.com/hyperyuri/repository-pattern-go/server"
 )
-
-type Server struct {
-	host   string
-	port   string
-	server *gin.Engine
-}
 
 func main() {
 	database.StartDatabase()
